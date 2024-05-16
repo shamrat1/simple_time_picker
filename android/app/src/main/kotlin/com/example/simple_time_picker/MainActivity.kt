1f475e55a8d8b70c27e5649ec1cc97b2d67e2a27
Yasin Siraj Shamrat <yshamrat@gmail.com>
2024-05-17 00:30:52 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/simple_time_picker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.simple_time_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ example/example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:simple_time_picker/main.dart';
+
+void main() {
+  runApp(MyApp());
+}
